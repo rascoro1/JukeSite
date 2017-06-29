d430f233f8cd78daf18fff96d1443ae541356466
rascoro1 <andcope1995@gmail.com>
2017-06-28 23:02:26 -0400

    Moving to master slave architecture

--- Components/test_start_sublime_from_cbm_to_ibc.py
+++ Components/test_start_sublime_from_cbm_to_ibc.py
@@ -14,5 +14,5 @@ ibc1.add_song(res[1])
 ibc1.add_song(res[2])
 
 ibc1.pop_song()
-ibc1.current_song.download()
-ibc1.current_song.play()
+print(ibc1.current_song.download())
+print(ibc1.current_song.play())
