12f3a6915c0e93e54436aa1ce89080ca06210904
rascoro1 <andcope1995@gmail.com>
2017-08-06 14:00:37 -0400

    Moving to master slave architecture

--- DjangoJukeSite/DjangoJukeSite/CBM.py
+++ DjangoJukeSite/DjangoJukeSite/CBM.py
@@ -405,6 +405,7 @@ def get_queue_songs(room_id):
     queue_songs = []
     songs = Queue.objects.filter(room_id=room_id)
     for s in songs:
+        print("Song storeId: {}".format(s.storeId))
         song_info = Track.objects.get(storeId=s.storeId)
         queue_songs.append(song_info)
     return queue_songs
